56d60e5fcb402c01fcc8aec4a9057e1915a00380
jwhartonranken <77158130+jwhartonranken@users.noreply.github.com>
2022-02-07 09:16:35 -0600

    Condensed open and close form down to toggleForm
    This fork shows how you can use javascript to check for css styling. Using this method, you only need one function to open or close the hex tab info. (Note: before this, I had a global boolean title isOpen that would toggle true or false whenever the popup opened or closed. The issue with this method is that, in theory, you'd have to create a boolean for each hex (Note within note: just realized this is not entirely true? You could have a global boolean titled hexIsOpen that toggled whenever ANY hex is open and go that route.))

--- functions.js
+++ functions.js
@@ -1,7 +1,19 @@
-function openForm() {
-    document.getElementById("mountain").style.display = "block";
+"using strict"
+let isOpen = false;
+
+window.addEventListener('load', (event) => {
+  document.getElementById("mountain").style.display = "none";
+})
+    
+
+toggleForm = (hex) => {
+  let displayValue = document.getElementById(hex).style.display;
+  console.log(displayValue);
+
+  if(displayValue == "none"){
+    document.getElementById(hex).style.display = "block";
   }
-  
-  function closeForm() {
-    document.getElementById("mountain").style.display = "none";
+  else if (displayValue == "block"){
+    document.getElementById(hex).style.display = "none";
   }
+}
